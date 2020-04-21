62deb02436d55a178ceeb6511ca7c369812029ac
andrey <mirashandrei@gmail.com>
2020-04-21 12:03:44 +0300

    all update ver 1.1

--- markup-new/src/blocks/banners/script.js
+++ markup-new/src/blocks/banners/script.js
@@ -3,14 +3,13 @@ import Swiper from 'swiper';
 var swiper = new Swiper('.swiper-banners', {
     pagination: {
         el: '.swiper-pagination',
-        freeMode: !1,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
         clickable: true,
         renderBullet: function (index, className) {
             return '<span class="' + className + '">' + '</span>';
         },
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 });
